d5c841a9d0f4624c4117c9e379c8ea65ff845fdb
Martin Pernollet <mpernollet@octo.com>
2014-09-28 11:05:24 +0200

    cleanup demos

--- aubio-demo-onset.R
+++ aubio-demo-onset.R
@@ -4,10 +4,8 @@ library(seewave)
 library(tuneR)
 
 fileBeatbox <- "data/beatbox.wav";
-fileDoremi <- "data/doremi.wav";
 
-#waveBeatbox <- readWave(fileBeatbow)
-#waveDoremi <- readWave(fileDoremi)
+waveBeatbox <- readWave(fileBeatbox)
 #play(a)
 
 # -------------------------
@@ -21,10 +19,6 @@ onsets <- aubioonset(fileBeatbox, "-O mkl -t 0.47")
 onsetPoints <- cbind(onsets, c(1:length(onsets))*0)
 
 # show onsets with points on AMPLITUDE
-oscillo(beatbox)#, type="p", cex=0.1)
+oscillo(waveBeatbox)#, type="p", cex=0.1)
 points(onsetPoints, col="red", cex=0.3)
 points(beatPoints, col="yellow", cex=0.3)
-
-# show onsets on STEREO plot
-plot(beatbox)
-points(onsetPoints, col="red", cex=0.3)
--- aubio-demo-pitch.R
+++ aubio-demo-pitch.R
@@ -3,25 +3,17 @@ source("aubio.R")
 library(seewave)
 library(tuneR)
 
-
 fileSinramp <- "data/sin-ramp.wav";
-fileBeatbox <- "data/beatbox.wav";
-fileDoremi <- "data/doremi.wav";
-
-#waveBeatbox <- readWave(fileBeatbox)
-#waveDoremi <- readWave(fileDoremi)
-#waveSinramp <- readWave(fileSinramp)
-#play(a)
 
 # ----------------------
-# PITCH ON DOREMI
+# PITCH
 
 # -p pitch detection algorithm
 # -s silence threshold [default=-70]
 pitch <- aubiopitch(fileSinramp, paste("-p", aubio.pitch.p[4]))#, "-s", -30)) # 4 mcomb 6 yinfft
-plot(pitch, type='p')#'p'
+plot(pitch, type='p')
 
-# ignore freq out of range
+# ignore freq out of range and out of 1s time window
 freqRangeHz = c(note.C0, note.C4)
 freqRangeKHz = freqRangeHz/1000
 timeRangeS = c(0,1)
@@ -29,57 +21,3 @@ timeRangeS = c(0,1)
 pitchFilter = filterFreqRange(pitch, freqRangeHz)
 pitchFilter = filterTimeRange(pitchFilter, timeRangeS)
 plot(pitchFilter, type='l')#'p'
-
-# compare all algorithms
-# par(mfcol=c(length(aubio.pitch.p),1))
-# for(i in aubio.pitch.p){
-#   print(i)
-#   pitch <- aubioPitch(fileDoremi, paste("-p",i))
-#   print(pitch)
-#   plot(pitch, type='p')#'p'
-# }
-
-wave = waveDoremi
-input<-inputw(wave=wave,f=f) ; wave<-input$w ; f<-input$f ; rm(input)
-
-from = NULL
-to = NULL
-if(is.null(from) && is.null(to)) {a<-0; b<-length(wave); from<-0; to<-length(wave)/f}
-if(is.null(from) && !is.null(to)) {a<-1; b<-round(to*f); from<-0}
-if(!is.null(from) && is.null(to)) {a<-round(from*f); b<-length(wave); to<-length(wave)/f}
-if(!is.null(from) && !is.null(to))
-{
-  if(from>to) stop("'from' cannot be superior to 'to'")
-  if(from==0) {a<-1} else {a<-round(from*f)}
-  b<-round(to*f)
-}
-wave<-as.matrix(wave[a:b,])
-n<-nrow(wave)
-amplitude <- abs(wave)
-
-# subsample ampltude to ensure no more events than nsamples
-nsamples = 10000
-selection = seq(from=1, to=length(amplitude), by=length(amplitude)/nsamples)
-amplitudeSummary = amplitude[selection]
-elapsed = selection / 44100
-amplitudeOut = cbind(elapsed, amplitudeSummary)
-
-ao = data.frame(amplitudeOut)
-ao[,1] <- format(ao[,1], scientific = FALSE)
-write.csv(ao, file = "data/doremi-amplitude.csv", quote = FALSE, row.names=FALSE)
-
-
-plot(amplitudeSummary, type='l')
-oscillo(waveDoremi)
-
-# ----------------
-## Seewave : Dominant frequency  
-spectro(waveDoremi, ovlp=75, zp=8, palette=rev.gray.colors.1, scale=FALSE, flim=freqRangeKHz)
-par(new=TRUE)
-#dfreq(waveDoremi, ovlp=50, threshold=6, type="l", col="red", lwd=2)
-
-## Seewave : Instantaneous frequency
-ifreq(waveDoremi, threshold=6, col="darkviolet", main="Instantaneous frequency with Hilbert transform")
-
-## Seewave : Spectrogram
-spectro(waveDoremi, ovlp=50, zp=16, collevels=seq(-40,0,0.5), flim=freqRangeKHZ)
